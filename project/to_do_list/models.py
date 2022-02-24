db6b0d2f626d45d48064a967954657501869ddac
Nikola Krajinovic <nikolakrajinovic@Nikolas-MacBook-Pro.local>
2022-02-24 14:52:36 +0100

    added missing user foreign key in todo model

--- project/to_do_list/models.py
+++ project/to_do_list/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class ToDo(models.Model):
     title = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     LOW = "low"
     MEDIUM = "med"
     HIGH = "hi"
